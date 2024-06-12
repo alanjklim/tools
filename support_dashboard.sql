ea828dfeda84c9778f88f074c218ab22dbac1f42
Alan Lim <39214849+alanjklim@users.noreply.github.com>
2024-06-12 14:36:03 +0800

    Update support_dashboard.sql

--- support_dashboard.sql
+++ support_dashboard.sql
@@ -1,100 +1,112 @@
-WITH Base AS (
-    SELECT
-        el.etl_id,
-        el.load_details,
-        el.system,
-        el.STATUS AS job_status,
-        el.schedule,
-        el.load_type,
-        el.autosys_job_pref,
-        el.stream_key,
-        elr.business_date_aet,
-        elr.STATUS AS load_status,
-        elr.run_id,
-        elr.insert_date AS load_insert_date,
-        elr.update_date AS load_update_date,
-        elr.update_date - elr.insert_date AS execution_time,
-        elra.attempt_id,
-        elra.status AS run_attempt_status,
-        elra.insert_date AS attempt_insert_date,
-        elra.update_date AS attempt_update_date,
-        ROW_NUMBER() OVER (PARTITION BY el.etl_id, elra.run_id ORDER BY elra.attempt_id DESC) AS rn,
-        MAX(CASE WHEN elra.status = 'SUCCESS' THEN elra.update_date END) OVER (PARTITION BY el.etl_id) AS last_success_date,
-        MAX(CASE WHEN elra.status = 'FAILED' THEN elra.update_date END) OVER (PARTITION BY el.etl_id) AS last_failed_date,
-        CASE 
-            WHEN MAX(elra.update_date) OVER (PARTITION BY el.etl_id) < TRUNC(SYSDATE) - 3 THEN 'YES'
-            ELSE 'NO'
-        END AS is_inactive_for_3_days
-    FROM omnia_op_meta_prod_owner.etl_load_runs elr
-    JOIN omnia_op_meta_prod_owner.etl_load_run_attempts elra ON elra.run_id = elr.run_id
-    JOIN omnia_op_meta_prod_owner.etl_loads el ON el.etl_id = el.etl_id
-    WHERE el.etl_id = 'BCB_AK2T14J1_INPTNRTNX_LOAD' -- Example filter, adjust as necessary
-),
-MetricsAndSchedules AS (
-    SELECT
-        els.etl_id,
-        els.sla_breach_time,
-        elrm.data_location,
-        elrm.observed,
-        elrm.expected
-    FROM omnia_op_meta_prod_owner.etl_load_schedules els
-    LEFT JOIN omnia_op_meta_prod_owner.ETL_LOAD_RUN_METRICS elrm ON els.etl_id = elrm.etl_id
-    WHERE elrm.metric_type_id = 1 AND elrm.stage_id = 2
+select * 
+from OMNIA_OP_META_PROD_OWNER.ETL_LOAD_DEPENDENCIES
+where etl_id ='BCB_AK2T1111_CKP_TRANSFORM_DIL';
+
+with base as (
+    select
+        coalesce(dep.etl_id, load.etl_id) AS etl_id
+        , dep.dependency_etl_id
+        , load.system
+        , load.load_details
+        , load.status
+        , load.schedule
+        , load.load_type 
+        , load.autosys_job_pref
+        , load.stream_key
+        , max(elr.run_id) as run_id
+        , max(elr.business_date_aet) as business_date_aet
+        , max(elr.insert_date) as load_insert_timestamp
+        , max(elr.update_date) aS load_update_timestamp
+        , initcap(max(case when elr.STATUS = 'NOTREADY' then 'NOT READY' else elr.STATUS end) KEEP (dense_rank last ORDER BY run_id)) as load_status
+    FROM OMNIA_OP_META_PROD_OWNER.ETL_LOADS load
+    LEFT JOIN OMNIA_OP_META_PROD_OWNER.ETL_LOAD_DEPENDENCIES dep ON dep.ETL_ID = load.ETL_ID 
+    left join OMNIA_OP_META_PROD_OWNER.etl_load_runs elr
+        ON elr.etl_id = load.etl_id
+        AND trunc(elr.business_date_utc) >= add_months(trunc(sysdate), - 1)
+    group by 
+        coalesce(load.ETL_ID, dep.ETL_ID)
+        , dep.dependency_etl_id
+        , load.system
+        , load.load_details
+        , load.status
+        , load.schedule
+        , load.load_type
+        , load.autosys_job_pref
+        , load.stream_key
 )
-SELECT
-    b.etl_id,
-    b.load_details,
-    b.system,
-    b.job_status,
-    b.schedule,
-    b.load_type,
-    b.autosys_job_pref,
-    b.stream_key,
-    b.business_date_aet,
-    b.load_status,
-    b.run_id,
-    ms.sla_breach_time,
-    b.load_insert_date,
-    b.load_update_date,
-    b.execution_time,
-    b.last_success_date,
-    b.last_failed_date,
-    b.is_inactive_for_3_days,
-    ms.data_location,
-    ms.observed,
-    ms.expected,
-    MAX(b.attempt_id) AS last_attempt_id,
-    COUNT(b.run_attempt_status) AS run_attempt_count,
-    SUM(CASE WHEN b.run_attempt_status = 'FAILED' THEN 1 ELSE 0 END) AS failed_count,
-    SUM(CASE WHEN b.run_attempt_status = 'NOTREADY' THEN 1 ELSE 0 END) AS notready_count,
-    SUM(CASE WHEN b.run_attempt_status = 'SKIPPED' THEN 1 ELSE 0 END) AS skipped_count,
-    SUM(CASE WHEN b.run_attempt_status = 'RUNNING' THEN 1 ELSE 0 END) AS running_count,
-    SUM(CASE WHEN b.run_attempt_status = 'SUCCESS' THEN 1 ELSE 0 END) AS success_count
-FROM Base b
-LEFT JOIN MetricsAndSchedules ms ON b.etl_id = ms.etl_id
-WHERE b.rn = 1
-GROUP BY
-    b.etl_id,
-    b.load_details,
-    b.system,
-    b.job_status,
-    b.schedule,
-    b.load_type,
-    b.autosys_job_pref,
-    b.stream_key,
-    b.business_date_aet,
-    b.load_status,
-    b.run_id,
-    ms.sla_breach_time,
-    b.load_insert_date,
-    b.load_update_date,
-    b.execution_time,
-    b.last_success_date,
-    b.last_failed_date,
-    b.is_inactive_for_3_days,
-    ms.data_location,
-    ms.observed,
-    ms.expected
-ORDER BY
-    b.business_date_aet DESC,
-    b.run_id DESC;
+, upstream as (
+    select
+        connect_by_root etl_id as root_etl_id
+        , dependency_etl_id
+        , level as layer
+        , system
+        , load_details
+        , status
+        , schedule
+        , load_type
+        , autosys_job_pref
+        , stream_key
+        , business_date_aet
+        , load_insert_timestamp
+        , load_update_timestamp
+        , load_status
+    from base
+    start with etl_id in (
+        select etl_id 
+        from base --use base or OMNIA_OP_META_PROD_OWNER.ETL_LOADS?
+        where system IN ('BCB', 'BCB', 'LNT', 'BRM', 'BCP', 'BGS', 'BLM', 'BCP', 'BHL', 'BCC', 'BAT', 'BTM', 'BAA', 'BAB', 'BLP', 'BSF', 'BQL', 'BIB', 'BSN'))
+    connect by nocycle prior dependency_etl_id = etl_id
+    --where CONNECT_BY_ISCYCLE = 0
+)
+, downstream as (
+    select
+        connect_by_root dependency_etl_id as root_etl_id
+        , etl_id
+        , level as layer
+        , system
+        , load_details
+        , status
+        , schedule
+        , load_type
+        , autosys_job_pref
+        , stream_key
+        , business_date_aet
+        , load_insert_timestamp
+        , load_update_timestamp
+        , load_status
+    from base
+    start with dependency_etl_id in (select dependency_etl_id from upstream) 
+    connect by nocycle dependency_etl_id = prior etl_id
+    --where CONNECT_BY_ISCYCLE = 0
+)
+select
+    us.root_etl_id as "ETL ID"
+    , us.dependency_etl_id as "Upstream ETL ID"
+    , us.system as "Upstream Source System"
+    , us.load_details as "Upstream Omnia Layer"
+    , us.status as "Upstream Job Status"
+    , us.schedule as "Upstream Schedule Frequency"
+    , us.load_type as "Upstream Load Type"
+    , us.autosys_job_pref as "Upstream Autosys Job"
+    , us.stream_key as "Upstream Stream Key"
+    , us.business_date_aet as "Upstream Business Date AET"
+    , us.load_insert_timestamp as "Upstream Load Insert Timestamp"
+    , us.load_update_timestamp as "Upstream Load Update Timestamp"
+    , us.load_status as "Upstream Load Status"
+    , us.layer as "Upstream Level"
+    , ds.etl_id as "Downstream ETL ID"
+    , ds.system as "Downstream Source System"
+    , ds.load_details as "Downstream Omnia Layer"
+    , ds.status as "Downstream Job Status"
+    , ds.schedule as "Downstream Schedule Frequency"
+    , ds.load_type as "Downstream Load Type"
+    , ds.autosys_job_pref as "Downstream Autosys Job"
+    , ds.stream_key as "Downstream Stream Key"
+    , ds.business_date_aet as "Downstream Business Date AET"
+    , ds.load_insert_timestamp as "Downstream Load Insert Timestamp"
+    , ds.load_update_timestamp as "Downstream Load Update Timestamp"
+    , ds.load_status as "Downstream Load Status"
+    , ds.layer as "Downstream Level"
+from upstream us
+left join downstream ds on us.root_etl_id = ds.root_etl_id
+    and us.dependency_etl_id is null
